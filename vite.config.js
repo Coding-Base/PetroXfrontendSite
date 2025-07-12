2a2c37961eef8ba4e95de413b26c27c21c1d715c
Osimi Godsgift <114400012+Coding-Base@users.noreply.github.com>
2025-07-12 10:13:56 +0100

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,13 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
-    outDir: 'dist',
-    // Add rollupOptions with external configuration
-    rollupOptions: {
-      external: [
-        '@react-google-maps/api'  // Mark this package as external
-      ]
-    }
+    outDir: 'dist'
   },
   resolve: {
     alias: {
-      // Remove the trailing slash
       '@': path.resolve(__dirname, 'src')
     }
   }
